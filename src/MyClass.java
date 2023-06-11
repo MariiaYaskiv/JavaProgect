279902db1a9fb59bfccf9f4d6be88222c1f1afe2
Mariya <lytvyn.mariia@gmail.com>
2023-06-11 20:34:03 +0300

    added my first homework

---
+++ src/MyClass.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    private int year = 2023;
+
+    public static int myMethod(int a, int b) {
+        int c = a + b; // sum of 2 numbers
+        return c;
+    }
+
+    public static void main(String[] args) {
+        myMethod(num1, num2);
+    }
+}
